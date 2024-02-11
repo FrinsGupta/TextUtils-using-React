471cb451fe60cf7cb65281d32e150e3b14d44694
Frins Gupta <pryncegupta@gmail.com>
2024-02-11 23:44:42 +0530

    Fixed a small bug

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         {/* <nav className={`navbar navbar-expand-lg navbar-dark bg-dark`}> */}
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/Textform">
+          <Link className="navbar-brand" to="/">
             {props.title}
           </Link>
           <button
